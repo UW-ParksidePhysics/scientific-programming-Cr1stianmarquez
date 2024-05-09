fda6c4f028d00c57e555b854359217774eee51a7
Replit user <>
2024-05-09 04:24:33 +0000

    Final

---
+++ final/calculate_lowest_eigenvectors.py
@@ -0,0 +1,17 @@
+""" This code produces the lowest eigenvalues and eigenvectors given a square matrix."""
+
+__author__ = 'Olivia Manring'
+
+
+import numpy as np
+
+def calculate_lowest_eigenvectors(square_matrix, number_of_eigenvectors=3):
+    eigenvalues, eigenvectors = np.linalg.eig(square_matrix)
+    return eigenvalues[:number_of_eigenvectors], eigenvectors[:, :number_of_eigenvectors]
+
+if __name__ == "__main__":
+    test_matrix = [[2, -1], [-1, 2]]
+    number_test_eigenvectors = 2
+    eigenvalues, eigenvectors = calculate_lowest_eigenvectors(test_matrix, number_test_eigenvectors)
+    print(f'eigenvalues = {eigenvalues}')
+    print(f'eigenvectors = {eigenvectors}')
